f94e964c5594ef1ffd3d23f3c923a21e54860fd0
Paulo Henrique <contato@paulohenriquesn.com>
2024-03-10 15:29:44 -0300

    Adicionar uma maquina EC2 na Subnet Pública

---
+++ computers.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "maquina-1" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.minha-subrede-publica.id
+  tags = {
+    Name = "maquina-1"
+  }
+}
